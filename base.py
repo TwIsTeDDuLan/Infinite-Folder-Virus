a66040611f2fd0a1c64692241b5a99e4b19b6c69
TwIsTeDDuLan <thulanadj@gmail.com>
2024-05-20 00:19:44 +0530

    methode 2 First TIME that i implimented proper recursive funtion!

--- base.py
+++ base.py
@@ -30,30 +30,28 @@ def list_drives():
     return drives
 
 def list_file(PATH):
-    folders = [folder for folder in os.listdir(PATH) if os.path.isdir(os.path.join(PATH, folder)) and folder[0] != '$']
-    return folders
+    try:
+        folders = [folder for folder in os.listdir(PATH) if os.path.isdir(os.path.join(PATH, folder)) and folder[0] != '$']
+        return folders
+    except:
+        return ''
 
 def file_cruser(PATH):
     folders = list_file(PATH)
-    newPATH = PATH
 
     if len(folders) > 0:
         for folder in folders:
+            newPATH = PATH
             newPATH += '/' + folder
+            print("Currently in:\n",newPATH)
             file_cruser(newPATH)
-        print(newPATH)
+            
 
     else:
-        delimiter = "/"
-        PathForDuplication = newPATH
-        delimiter_count = PathForDuplication.count(delimiter)
-
-        for i in range(delimiter_count):
-            print(PathForDuplication)
-            ListForDuplication = PathForDuplication.rsplit("/", 1)
-            NewPathForDuplication = ListForDuplication[0]
-            Duplicate(NewPathForDuplication)
-            PathForDuplication = NewPathForDuplication
+        newPathList = PATH.rsplit('/',1)
+        Path = newPathList[0]
+        Duplicate(Path)
+        print("Duplicated:\n",Path)
 
 
 
