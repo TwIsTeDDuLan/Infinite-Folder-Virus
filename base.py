4f6a1e2273459ba3ebcafbff9fb21a18140e4cca
TwIsTeDDuLan <thulanadj@gmail.com>
2024-05-20 01:30:40 +0530

    Further investigations
    implimented a folder maker to help

--- base.py
+++ base.py
@@ -13,7 +13,7 @@ import random
 
 def main():
     drives = list_drives()
-    PATH = 'E:/'
+    PATH = 'E:/Abb'
     #for drive in drives:
     #PATH += drive
     file_cruser(PATH)
@@ -45,17 +45,32 @@ def file_cruser(PATH):
             newPATH += '/' + folder
             print("Currently in:\n",newPATH)
             file_cruser(newPATH)
-            
+            Duplicate(PATH,folder)
 
     else:
         newPathList = PATH.rsplit('/',1)
         Path = newPathList[0]
-        Duplicate(Path)
-        print("Duplicated:\n",Path)
+        FolderName = newPathList[1]
+        Duplicate(Path,FolderName)
 
+def Duplicate(PATH,Name):
+    print("Duplicated:\n",PATH,f"({Name})")
+    duplicatedFolders = []
+    os.chdir(PATH)
+    
+    duplicates = 5
+    for i in range(duplicates):
+        newName = f"{Name}{i}"
+        try:
+            os.mkdir(newName)
+            duplicatedFolders.append(newName)
+        except:
+            pass
 
-
-def Duplicate(PATH):
-    return
+    max = len(duplicatedFolders) - 1
+    index = random.randint(0,max)
+    src = PATH + '/' + Name
+    dest = PATH + '/' + duplicatedFolders[index]
+    destination = shutil.move(src,dest)
 
 main()
--- test.py
+++ test.py
@@ -1,5 +1,35 @@
+
+
 import os
 
-folders = [folder for folder in os.listdir("E:/") if os.path.isdir(os.path.join("E:/", folder))]
+def FolderRemove():
+    os.chdir("E:/Abb")
+    folders = [folder for folder in os.listdir("E:/Abb")]
+    for folder in folders:
+        os.remove(f"E:/Abb/{folder}")
+    return
 
-print(folders)
+def folderMaker():
+    os.chdir("E:/Abb")
+    for i in range(4):
+        os.mkdir(chr(97+i))
+    
+    os.chdir("E:/Abb/a")
+
+    for i in range(3):
+        os.mkdir(chr(97+i))
+
+
+def main():
+    inp = int(input("what want?"))
+    if inp == 1:
+        folderMaker()
+    else:
+        FolderRemove()
+
+try:
+    while True:
+        main()
+except:
+    while True:
+        main()
